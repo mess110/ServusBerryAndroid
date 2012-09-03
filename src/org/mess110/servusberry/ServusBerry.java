4a62798bf72cc62c41c22c00aaf728cbb149eacd
Cristian Mircea Messel <mess110@gmail.com>
2012-09-04 00:47:40 +0300

    find wifi password

--- src/org/mess110/servusberry/ServusBerry.java
+++ src/org/mess110/servusberry/ServusBerry.java
@@ -6,9 +6,14 @@ import org.mess110.servusberry.util.API;
 import org.mess110.servusberry.util.Preferences;
 import org.mess110.servusberry.util.Util;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+// TODO: fix this liek FileSystemActivity
+// evaluate if there should be an API object here, since API object
+// only does a HTTP request
 public class ServusBerry extends BaseActivity {
 	private API api;
 	private Preferences pref;
@@ -26,16 +31,39 @@ public class ServusBerry extends BaseActivity {
 		api = new API(getApplicationContext());
 	}
 
+	private String getIpAddr() {
+		WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+		int ip = wifiInfo.getIpAddress();
+
+		String ipString = String.format("%d.%d.%d.%d", (ip & 0xff),
+				(ip >> 8 & 0xff), (ip >> 16 & 0xff), (ip >> 24 & 0xff));
+
+		return ipString.toString();
+	}
+
 	@Override
 	public void detectServer() {
+		for (int i = 1; i <= 255; i++) {
 
+			String url = "http://192.168.1." + String.valueOf(i) + ":5000/";
+			try {
+				String jsonString = Util.executeHttpGet(url, true);
+				if (ServerDetect.isServer(jsonString)) {
+					pref.setUrl(url);
+					break;
+				}
+			} catch (Exception e) {
+				// e.printStackTrace();
+			}
+		}
+		servusBerryServer.setText(pref.getUrl());
+		Util.toast(getApplicationContext(), getIpAddr());
 	}
 
 	@Override
 	public void ping() {
-		servusBerryServer.setText(pref.getUrl());
 		String result = api.ping(pref.getUrl());
-		Util.log(result);
 		if (ServerDetect.isServer(result)) {
 			Util.toast(getApplicationContext(), "connected");
 		} else {
--- src/org/mess110/servusberry/util/API.java
+++ src/org/mess110/servusberry/util/API.java
@@ -1,8 +1,5 @@
 package org.mess110.servusberry.util;
 
-import org.apache.http.conn.ConnectTimeoutException;
-import org.json.JSONObject;
-
 import android.content.Context;
 
 // This can be dried up even further
@@ -32,26 +29,6 @@ public class API {
 			result = Util.executeHttpPost(url);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		return result;
-	}
-
-	public String findServusBerryServer() {
-		String result = "";
-		for (int i = 1; i <= 255; i++) {
-			String url = "http://192.168.1." + String.valueOf(i) + ":5000/";
-			try {
-				String jsonString = Util.executeHttpGet(url, true);
-				JSONObject response = new JSONObject(jsonString);
-				if (response.has("servusberry")) {
-					result = url;
-					break;
-				}
-			} catch (ConnectTimeoutException e) {
-				// e.printStackTrace();
-			} catch (Exception e) {
-				// e.printStackTrace();
-			}
 		}
 		return result;
 	}
